2f22ae5cfe619ffe6124433f58f7979a1e015d8a
Jacob <mrjtart@yahoo.com>
2023-01-07 15:19:11 -0800

    assignments 1 & 2 done

---
+++ prepare/Prep01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Prep01
+{
+    static void Main(string[] args)
+    {
+        string firstName;
+        string lastName;
+
+        Console.WriteLine("Please enter your first and last name (one space, no commas)");
+        string[] fullName = Console.ReadLine().Split(" ");
+
+        firstName = fullName[0];
+        lastName = fullName[1];
+
+        Console.WriteLine($"\nYour name is {lastName}, {firstName} {lastName}");
+    }
+}
